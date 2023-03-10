7d7917c93b2aecab2d73b5ef2e36ac4eaab922d6
Vincent Lewi <vincentlewii@gmail.com>
2023-03-10 18:22:07 +0800

    connected product to MongoDB

--- product.py
+++
@@ -1,9 +0,0 @@
-from flask import Flask
-from pymongo import MongoClient
-
-app = Flask(__name__)
-
-client = MongoClient('localhost', 27017)
-
-db = client.flask_db
-todos = db.todos
--- product/routes.py
+++ product/routes.py
@@ -1,6 +1,9 @@
 from flask import Flask, request, jsonify
 from flask_pymongo import PyMongo
 from pymongo import MongoClient
+import json
+from bson import json_util
+from bson.json_util import ObjectId
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'aad91c65aee579cd33ffb46e37c5b27d8a25a580'
@@ -9,13 +12,22 @@ app.config['MONGO_URI'] = "mongodb+srv://vincentlewi:ZYjoiyuH1YJcB0kG@syopi.ks36
 # setup mongodb
 client = MongoClient("mongodb+srv://vincentlewi:ZYjoiyuH1YJcB0kG@syopi.ks3652i.mongodb.net/?retryWrites=true&w=majority")
 
+class MyEncoder(json.JSONEncoder):
+    def default(self, obj):
+        if isinstance(obj, ObjectId):
+            return str(obj)
+        return super(MyEncoder, self).default(obj)
+app.json_encoder = MyEncoder
+ 
 @app.route('/')
 
-def index():
+def index():    
     dbs = client.list_database_names()
+    # db = client['product']
+    # product = db['product']
     product = client.product
     collections = product.list_collection_names()
-    return collections
+    return client['product']['product'].find_one({'ProductName': 'bebek'})
 
 if __name__ == '__main__':
     app.run(port=5000, debug=True)
