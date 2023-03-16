8ee39ba635a0c1ad41b575542ad7d2d6ff95d935
Emily Aurelia <emilyaurelia01@gmail.com>
2023-03-16 13:54:28 +0800

    emil push

--- syopi-dashboard/src/components/ProductPage.tsx
+++ syopi-dashboard/src/components/ProductPage.tsx
@@ -1,8 +1,24 @@
 import React from 'react'
+import productpics from '../assets/logofornow.jpg'
+import './ProductPage.css'
+import {Container, Col, Row, Carousel, Button} from 'react-bootstrap'
 
 function ProductPage() {
   return (
-    <div>This is where u see one individual Product</div>
+    <div>
+      <Container>
+        <Row>
+          <Col>
+            <img src={productpics}/> 
+            {/* Should have been a carousel but idk why it doesnt work */}
+          </Col>
+          <Col>
+            <p className='product-title'>Product Name</p>
+          </Col>
+        </Row>
+      </Container>
+    </div>
+      
   )
 }
 
