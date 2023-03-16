5ab26167203c909c5ba1f5400ced6c1cc13b36aa
Vincent Lewi <vincentlewii@gmail.com>
2023-03-16 10:26:33 +0800

    Changed all ports to 5000

--- syopi-dashboard/src/components/Shop/ShopPage.tsx
+++ syopi-dashboard/src/components/Shop/ShopPage.tsx
@@ -11,7 +11,7 @@ function ShopPage(props: any) {
 
   async function fetchShop() {
     console.log(shopName)
-    fetch(`http://127.0.0.1:5001/shop/${shopName}`)
+    fetch(`http://127.0.0.1:5000/shop/${shopName}`)
       .then((response) => response.json())
       .then((data) => (setShop(data.data[0])))
   }
