ad06203ef32b214770b4617999b1b697abc5c155
VITTO SURYA TEDJA <vittosuryat.2021@scis.smu.edu.sg>
2023-03-18 19:02:52 +0800

    add basic functionality to cart and addtoCart btn

--- syopi-dashboard/src/components/Cart.tsx
+++ syopi-dashboard/src/components/Cart.tsx
@@ -1,26 +1,14 @@
-import React, { useState } from 'react'
-import Cart from './User'
-import './User'
+import { useState } from 'react'
 
-function CartList() {
-  const [cart, setCart] = useState(Array())
+function Cart() {
+  const [data, setData] = useState(Object)
 
-  function fetchData() {
-    fetch("http://127.0.0.1:5300s/")
-    .then((response) => response.json())
-    .then((data) => setCart(data))
-  }
-  
+  // const fetchData
   return (
-    <>
-    <div>Cart disini</div>
-    <div className='cart'>
-        <Cart/>
-        <Cart/>
-        <Cart/>
+    <div>
+    Hello World
     </div>
-    </>
   )
 }
 
-export default CartList
+export default Cart
