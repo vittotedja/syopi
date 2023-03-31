bae3f7e650446689ef54afd96c503496e719defc
Vincent Lewi <vincentlewii@gmail.com>
2023-03-31 16:52:16 +0800

    search bar created, not yet connected to Browse

--- syopi-dashboard/src/components/Navbar.tsx
+++ syopi-dashboard/src/components/Navbar.tsx
@@ -4,42 +4,16 @@ import logo from '../assets/logo.png'
 import { HiSearch, HiBell, HiShoppingCart, HiUser } from "react-icons/hi";
 import './Navbar.css'
 import { useNavigate } from 'react-router-dom';
+import SearchBar from './SearchBar';
 
 function Navbar() {
   let navigate = useNavigate()
-
-  function search() {
-    const data = {
-        ProductName: 1,
-        ShopId: 1,
-        Stock: 1,
-        Price: 1
-    }
-    fetch('http://127.0.0.1:5000/search', {
-        method:'POST',
-        headers: {
-          "Content-Type": "application/json"
-        },
-        body: JSON.stringify(data),
-    })
-    .then((response) => response.json())
-    .then((data) => {
-      console.log("Success:", data)
-    })
-    .catch((error) => {
-      console.error("Error:", error);
-    });
-  }
-
-  useEffect(() => {
-    search();
-  }, []);
-
+  
   return (
     <div className='navbar flex'>
         <img src={logo} alt="Logo" width={"60px"} onClick={() => navigate('/')} style={{cursor: 'pointer'}}/>
           <HiSearch className = "icon"/>
-          <input type="text" placeholder="Search" style = {{backgroundColor: '#FFD3B6'}}/>
+          <SearchBar components={{ DropdownIndicator:() => null, IndicatorSeparator:() => null }}/>
           <HiBell className="icon notif"/>
           <div onClick={() => navigate('/cart')}>
             <HiShoppingCart className="icon cart"/>
---
+++ syopi-dashboard/src/components/SearchBar.tsx
@@ -0,0 +1,70 @@
+import { useState } from 'react';
+import AsyncSelect from 'react-select/async';
+// import './SearchBar.css';
+
+function SearchBar(props: any) {
+
+  const searchMovies = async (inputValue: string) => {
+    if (inputValue) {
+      const response = await fetch(`http://127.0.0.1:5000/product/search/${inputValue}`)
+      const data = await response.json()
+      return data;  
+    }
+    return [];
+  };
+
+  const loadOptions = async (inputValue: string, callback: (options: any[]) => void) => {
+    const options = await searchMovies(inputValue);
+    callback(options);
+  };
+
+  const colourStyles = {
+    container: (style: Object) => ({...style,flex: 1}),
+    indicatorSeparator: (style: Object) => ({...style, display: 'none'}),
+    dropdownIndicator: (style: Object) => ({...style, display: 'none'}),
+    control: (style: Object) => ({ ...style, backgroundColor: '#333', color: '#fff' }),
+    singleValue: (style: Object) => ({...style, color: '#fff',}),
+    input: (style: Object) => ({...style, color: '#fff',}),
+    menu: (style: Object) => ({ ...style, backgroundColor: '#333', color: '#fff' }),
+    option: (style: Object, { isDisabled, isFocused, isSelected }) => {
+      const backgroundColor = isDisabled ? '#444' : isSelected ? '#666' : isFocused ? '#555' : '#333';
+      const color = isDisabled ? '#888' : isSelected ? '#ccc' : isFocused ? '#fff' : '#ccc';
+      return {
+        ...style,
+        backgroundColor,
+        color,
+        cursor: isDisabled ? 'not-allowed' : 'default'
+      };
+    },
+  };
+
+  let [rating, setRating] = useState(0) 
+  let [movie, setMovie] = useState({movieId: 0, title: ''})
+  let selectedMovie = {movieId: movie.movieId, title: movie.title, userRating: rating}
+
+  return (
+    <div className='search-bar'>
+      <AsyncSelect
+        cacheOptions
+        loadOptions={loadOptions}
+        defaultOptions
+        placeholder='Search for a movie...'
+        onChange={(selectedOption: Object) => {setMovie({movieId: selectedOption.value, title: selectedOption.label}), setRating(0)}}
+        styles={colourStyles}
+      />
+      <button 
+        disabled={!rating||!movie.movieId}
+        className='search-button' 
+        onClick={() => props.setSelectedMovies(
+          props.selectedMovies.find((movie: Object) => JSON.stringify(movie.title) === JSON.stringify(selectedMovie.title))
+          ? props.selectedMovies.filter((movie: Object) => JSON.stringify(movie.title) != JSON.stringify(selectedMovie.title))
+          : [...props.selectedMovies, selectedMovie], console.log(selectedMovie)
+        )}
+      >
+        Search
+      </button>
+    </div>
+  );
+}
+
+export default SearchBar;
