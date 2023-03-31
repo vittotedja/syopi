90eedc2b6a87d7e8225e5b03c226396f34357c0a
yozafard <yozafardhs.2021@scis.smu.edu.sg>
2023-04-01 00:45:55 +0800

    fix the bugs after merging

--- syopi-dashboard/src/App.tsx
+++ syopi-dashboard/src/App.tsx
@@ -1,31 +1,19 @@
 import React, { useState, useEffect } from "react";
 import "./App.css";
-import LandingPage from "./components/LandingPage";
+import LandingPage from "./components/Browse/LandingPage";
 import ShopPage from "./components/Shop/ShopPage";
 import { Routes, Route, BrowserRouter } from "react-router-dom";
-import ProductPage from "./components/ProductPage";
-import Review from "./components/Review";
+import ProductPage from "./components/Product/ProductPage";
+import Review from "./components/Review/Review";
 import User from "./components/User";
-import Cart from "./components/Cart";
+import Cart from "./components/Cart/Cart";
 import SignUp from "./components/auth/SignUp.jsx";
 import Login from "./components/auth/Login.jsx";
 import Homepage from "./components/auth/Homepage.jsx";
-import  PrivateRoute from "./components/auth/Priv.jsx";
-=======
-import React, { useState, useEffect } from 'react'
-import './App.css'
-import LandingPage from './components/Browse/LandingPage'
-import ShopPage from './components/Shop/ShopPage'
-import { Routes, Route, BrowserRouter } from 'react-router-dom'
-import ProductPage from './components/Product/ProductPage'
-import Review from './components/Review/Review'
-import User from './components/User'
-import Cart from './components/Cart/Cart'
-import SignUp from './components/Auth/SignUp'
-import Login from './components/Auth/Login'
-import Homepage from './components/Auth/Homepage'
-import SearchPage from './SearchPage'
-
+import PrivateRoute from "./components/auth/Priv.jsx";
+import { AuthProvider } from "./components/auth/Context.jsx";
+import "./App.css";
+import SearchPage from "./SearchPage";
 
 export default function App() {
   return (
@@ -37,31 +25,22 @@ export default function App() {
         <Route path="/product/:productId" element={<ProductPage />} />
         <Route path="/review" element={<Review />} />
         <Route element={<PrivateRoute />}>
-          <Route path="/user" element= {<User />}/> 
-          <Route path="/cart" element= {<Cart />}/> 
+          <Route path="/user" element={<User />} />
+          <Route path="/cart" element={<Cart />} />
           <Route path={"/homepage"} element={<Homepage />} />
         </Route>
         <Route path={"/signup"} element={<SignUp />} />
         <Route path="/login" element={<Login />} />
+
+        <Route path="/" element={<LandingPage />} />
+        <Route path="/shop/:shopName" element={<ShopPage />} />
+        <Route path="/product" element={<ProductPage />} />
+        <Route path="/:productId" element={<ProductPage />} />
+        <Route path="/review" element={<Review />} />
+        <Route path="/search" element={<SearchPage />} />
+        <Route path="/user" element={<User />} />
+        <Route path="/cart" element={<Cart />} />
       </Routes>
-
-      <BrowserRouter>
-        <Routes>
-          <Route path='/' element={<LandingPage />} />
-          <Route path='/shop/:shopName' element={<ShopPage />} />
-          <Route path='/product' element={<ProductPage />} />
-          <Route path= '/:productId' element={<ProductPage />} />
-          <Route path='/review' element={<Review />} />
-          <Route path='/search' element={<SearchPage />} />
-          <Route path='/user' element={<User/>} />
-          <Route path='/cart' element={<Cart/>} />
-
-          <Route path={'/signup'} element={ <SignUp />} />
-          <Route path={'/login'} element={ <Login setToken={setToken}/>} />
-          {token?<Route path={'/homepage'} element={ <Homepage token={token} />} />:""}
-        </Routes>
-      </BrowserRouter>
-
     </>
   );
 }
