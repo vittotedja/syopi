e73e9cdaa521d4ef0d8b40a8836535025e5951f8
Emily Aurelia <emilyaurelia01@gmail.com>
2023-04-01 11:35:28 +0800

    shipping change column name

--- shipping/shipping.js
+++ shipping/shipping.js
@@ -1,10 +1,10 @@
 function show_all(data){
     let text = ''
     for(shipping of data){
-        let id = shipping.id
-        let from_port = shipping.from_port
-        let to_port = shipping.to_port
-        text += id + ' ' + from_port + ' ' + to_port + '<br/>'
+        let ShippingId = shipping.ShippingId
+        let ShippingStatus = shipping.ShippingStatus
+        let Current = shipping.Current
+        text += ShippingId + ' ' + ShippingStatus + ' ' + Current + '<br/>'
     }
     return text
 }
@@ -20,21 +20,21 @@ fetch('http://127.0.0.1:5000/shipping')
 
 function addShipping() {
     console.log('addProduct()')
-    const id = document.querySelector('#id').value
-    const from_port = document.querySelector('#from_port').value
-    const to_port = document.querySelector('#to_port').value
-    const status = document.querySelector('#status').value
-    const courier = document.querySelector('#courier').value
-    const order_id = document.querySelector('#order_id').value
-    const current_port = document.querySelector('#current_port').value
+    const ShippingId = document.querySelector('#ShippingId').value
+    const ShippingStatus = document.querySelector('#ShippingStatus').value
+    const ShopAddress = document.querySelector('#ShopAddress').value
+    const Current = document.querySelector('#Current').value
+    const UserAddress = document.querySelector('#UserAddress').value
+    const CourierId = document.querySelector('#CourierId').value
+    const DriverId = document.querySelector('#DriverId').value
     const data = {
-        id: id,
-        from_port: from_port,
-        to_port: to_port,
-        status: status,
-        courier: courier,
-        order_id: order_id,
-        current_port: current_port
+        ShippingId: ShippingId,
+        ShippingStatus: ShippingStatus,
+        ShopAddress: ShopAddress,
+        Current: Current,
+        UserAddress: UserAddress,
+        CourierId: CourierId,
+        DriverId: DriverId
     }
     console.log(data)
     fetch('http://127.0.0.1:5000/shipping', {
