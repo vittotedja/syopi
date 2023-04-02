e4f587bb7bdccde5930d05e5bfcedd98d9ea1f1b
Vincent Lewi <vincentlewii@gmail.com>
2023-04-02 15:33:00 +0800

    product imageUrl done

--- syopi-dashboard/src/components/Browse/ProductsList.tsx
+++ syopi-dashboard/src/components/Browse/ProductsList.tsx
@@ -48,7 +48,17 @@ function ProductsList() {
       <br/>
       <button onClick={() => addProduct()}>add product</button>
       <div className="product-list">
-        {products.map((product) => {return <Products key={product.ProductId} ProductName={product.ProductName} stock = {product.Stock} AvgRating = {product.AvgRating} Price = {product.Price} id = {product.ProductId}/>})}
+        {products.map((product) => {
+          return <Products 
+            key={product.ProductId} 
+            ProductName={product.ProductName} 
+            stock={product.Stock} 
+            AvgRating={product.AvgRating} 
+            Price={product.Price} 
+            id={product.ProductId}
+            imageUrl={product.ImageUrls[0].ImageUrl}
+          />
+        })}
       </div>
     </div>
   );
