4cdb4553c447e146d9d3e09df9c23334c7d57ceb
Jordian Renaldi <jrenaldi.2021@scis.smu.edu.sg>
2023-06-18 18:20:52 +0700

    Changes to courier side
    Changes to courier side

--- syopi-dashboard/src/components/Courier/Courier.tsx
+++ syopi-dashboard/src/components/Courier/Courier.tsx
@@ -1,109 +1,204 @@
-import React, {useEffect,useState} from 'react';
+import React, { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import "./Courier.css";
-import Card from 'react-bootstrap/Card';
-import CourierCard from './CourierCard'
+import Card from "react-bootstrap/Card";
+import CourierCard from "./CourierCard";
 
 export default function Courier(props: any) {
-    const [orders, setOrders] = useState(Array)
-    const [shippingData, setShippingData] = useState<any>({});
-    const [shippingStatus, setShippingStatus] = useState<string>(props.ShippingStatus);
-    const [showNewShipping, setshowNewShipping] = useState(true)
-    const [showcurrentShipping, setshowcurrentShipping] = useState(false)
-    let navigate = useNavigate();
+  const [orders, setOrders] = useState(Array);
+  const [shippingData, setShippingData] = useState<any>({});
+  const [shippingStatus, setShippingStatus] = useState<string>(
+    props.ShippingStatus
+  );
+  const [showNewShipping, setshowNewShipping] = useState(true);
+  const [showcurrentShipping, setshowcurrentShipping] = useState(false);
+  const [showCompletedShipping, setShowCompletedShipping] = useState(false);
+  const [showReportedShipping, setShowReportedShipping] = useState(false);
+  const [header, setHeader] = useState("New Shipping");
+  let navigate = useNavigate();
 
-    function fetchData(){
-        fetch("http://127.0.0.1:5000/order/")
-    }
+  const mockNewOrderData = [
+    {
+      OrderId: "1",
+      Date: new Date(),
+      ShippingId: "34",
+      ProductName: "Lato-lato terbaru ! MURAH !! PROMO !!! ",
+      ProductId: "11421",
+      Quantity: "5",
+      Price: "1.00",
+      Total: "5.00",
+      UserAddress: "70 Stamford Road, 279383",
+      ShopAddress: "30 Dhoby Ghaut Plaza, 023321",
+      CourierId: "12312",
+      DriverId: "123144",
+      Status: "NEW",
+    },
+    {
+      OrderId: "2",
+      Date: new Date(),
+      ShippingId: "57",
+      ProductName: "Dog collar ! 3 different sizes and 10 colours !",
+      ProductId: "525246",
+      Quantity: "2",
+      Price: "10.00",
+      Total: "20.00",
+      UserAddress: "7 Toa Pa Yoh Road, 275233",
+      ShopAddress: "35 Punggol Street, 959403",
+      CourierId: "235264",
+      DriverId: "141435",
+      Status: "ACCEPTED",
+    },
+    {
+      OrderId: "2",
+      Date: new Date(),
+      ShippingId: "57",
+      ProductName: "Dog collar ! 3 different sizes and 10 colours !",
+      ProductId: "525246",
+      Quantity: "2",
+      Price: "10.00",
+      Total: "20.00",
+      UserAddress: "7 Toa Pa Yoh Road, 275233",
+      ShopAddress: "35 Punggol Street, 959403",
+      CourierId: "235264",
+      DriverId: "141435",
+      Status: "COMPLETED",
+    },
+  ];
 
- 
-    function getShippingData() {
-      fetch(`http://127.0.0.1:5000/find_by_shippingid/${props.ShippingId}`)
-        .then((response) => response.json())
-        .then((data) => setShippingData(data[0]));
-    }
+  function fetchData() {
+    fetch("http://127.0.0.1:5000/order/");
+  }
 
-    useEffect(() => {
-      getShippingData();
-    }, []);
-    
-
-    const acceptShipping = (shippingId: string) => {
-      const data = { ShippingId: shippingId };
-      fetch("http://127.0.0.1:5000/process_shipping/accept",
-        {
-          method: "POST",
-          headers: { "Content-Type": "application/json" },
-          body: JSON.stringify(data)
-        })
+  function getShippingData() {
+    fetch(`http://127.0.0.1:5000/find_by_shippingid/${props.ShippingId}`)
       .then((response) => response.json())
-      .then(() => setShippingStatus('Accepted'))
-    }
+      .then((data) => setShippingData(data[0]));
+  }
 
-    
+  useEffect(() => {
+    getShippingData();
+  }, []);
 
+
+  const toggleNewShipping = () => {
+    setHeader("New Shipping");
+    setshowNewShipping(true);
+    setshowcurrentShipping(false);
+    setShowCompletedShipping(false);
+    setShowReportedShipping(false);
+  };
+
+  const toggleCurrentShipping = () => {
+    setHeader("Current Shipping");
+    setshowNewShipping(false);
+    setshowcurrentShipping(true);
+    setShowCompletedShipping(false);
+    setShowReportedShipping(false);
+  };
+
+  const toggleCompletedShipping = () => {
+    setHeader("Completed Shipping");
+    setshowNewShipping(false);
+    setshowcurrentShipping(false);
+    setShowCompletedShipping(true);
+    setShowReportedShipping(false);
+  };
+
+  const toggleReportedShipping = () => {
+    setHeader("Reported Shipping");
+    setshowNewShipping(false);
+    setshowcurrentShipping(false);
+    setShowCompletedShipping(false);
+    setShowReportedShipping(true);
+  };
 
   return (
     <>
-    <div className='seller-container'>
-      <div className='row'>
-        <div className='col-12'>
-          <p className='seller-page-title'>Courier</p>
-        </div>
-      </div>
-    <div className='row' style={{margin:'0'}}>
-        <div className='col-3'>
-          <div className='seller-menu'>
-          
-          
-          <p><button onClick={()=>{setshowNewShipping(true), setshowcurrentShipping(false)}} className='seller-page'>New Shipping Order</button></p>
-          <p><button onClick={()=>{setshowcurrentShipping(true),setshowNewShipping(false)}} className='seller-page'>Current Shipping Order</button></p>
+      <div className="seller-container">
+        <div className="row">
+          <div className="col-12">
+            <p className="seller-page-title">Courier</p>
           </div>
         </div>
-        
-          
-          
-        <div className='col-9 output'>
-        {
-          (() => {
-              if (showNewShipping)
-                  return <>
-                  <div className='seller-menu'>
-                  <div className='row' style={{margin:'0'}}>
-                    
-                    <div className='col-12'>
-                    <p className='seller-page-title'>New Shipping Order</p>
+        <div className="row" style={{ margin: "0" }}>
+          <div className="col-3">
+            <div className="seller-menu">
+              <p>
+                <button onClick={toggleNewShipping} className="seller-page">
+                  New
+                </button>
+              </p>
+              <p>
+                <button onClick={toggleCurrentShipping} className="seller-page">
+                  Current
+                </button>
+              </p>
+              <p>
+                <button
+                  onClick={toggleCompletedShipping}
+                  className="seller-page"
+                >
+                  Completed
+                </button>
+              </p>
+              <p>
+                <button
+                  onClick={toggleReportedShipping}
+                  className="seller-page"
+                >
+                  Reported
+                </button>
+              </p>
+            </div>
+          </div>
+
+          <div className="col-9 output">
+            {(() => {
+              return (
+                <>
+                  <div className="seller-menu">
+                    <div className="row" style={{ margin: "0" }}>
+                      <div className="col-12">
+                        <p className="seller-page-title">{header}</p>
+                      </div>
                     </div>
+                    <hr />
                   </div>
-                  
-                  <hr/>
-                  </div>
-                  <CourierCard shippingData = {shippingData} shippingStatus = {shippingStatus}/>
-                </>
-              else (showcurrentShipping)
-                  return <>
-                  <div className='seller-menu'>
-                  <div className='row' style={{margin:'0'}}>
-                    
-                    <div className='col-12'>
-                    <p className='seller-page-title'>Current Shipping Order</p>
-                    </div>
-                  </div>
-                  <hr/>
-                  </div>
-                  <CourierCard shippingData = {shippingData} shippingStatus = {shippingStatus}/>
+                  {mockNewOrderData.map((order) => {
+                    return (
+                      <CourierCard
+                        key={order.OrderId + order.ShippingId}
+                        OrderId={order.OrderId}
+                        DateObj={order.Date}
+                        ShippingId={order.ShippingId}
+                        ProductName={order.ProductName}
+                        ProductId={order.ProductId}
+                        Quantity={order.Quantity}
+                        Price={order.Price}
+                        Total={order.Total}
+                        UserAddress={order.UserAddress}
+                        ShopAddress={order.ShopAddress}
+                        CourierId={order.CourierId}
+                        DriverId={order.DriverId}
+                        Status={order.Status}
+                      />
+                    );
+                  })}
+                  {/* <CourierCard
+                     shippingData={shippingData}
+                     shippingStatus={shippingStatus}
+                   /> */}
+
                   {/* {orderData.map((order:any) => {
                     console.log(order)
                     return <SellerOrder key={order[0].OrderId} orderData={order}/>
                   })} */}
-                  </>
-              
-          })()
-        }
-        </div></div>
-    </div>
+                </>
+              );
+            })()}
+          </div>
+        </div>
+      </div>
     </>
-
-  )
+  );
 }
- 
